2fa19c4e359dcb501fd1832a79ccada1b90c18fb
Mark-DSouza <markdsouza434@gmail.com>
2021-04-11 10:14:39 +0530

    Commit fully functional tab-bar made using BottomNavigationBar

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -9,24 +9,68 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoryScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Your Favorites',
+    },
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      // initialIndex: 0,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Meals"),
-          bottom: TabBar(tabs: [
-            Tab(icon: Icon(Icons.category), text: 'Categories',),
-            Tab(icon: Icon(Icons.star), text: 'Favorite',),
-          ],),
-        ),
-        body: TabBarView(children: [
-          CategoryScreen(),
-          FavoritesScreen(),
-        ],)
+    return Scaffold(
+      appBar: AppBar(title: Text(_pages[_selectedPageIndex]['title'])),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.category,
+            ),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.star,
+            ),
+            label: 'Favorites',
+          )
+        ],
       ),
     );
+    // return DefaultTabController(
+    //   length: 2,
+    //   // initialIndex: 0,
+    //   child: Scaffold(
+    //     appBar: AppBar(
+    //       title: Text("Meals"),
+    //       bottom: TabBar(tabs: [
+    //         Tab(icon: Icon(Icons.category), text: 'Categories',),
+    //         Tab(icon: Icon(Icons.star), text: 'Favorite',),
+    //       ],),
+    //     ),
+    //     body: TabBarView(children: [
+    //       CategoryScreen(),
+    //       FavoritesScreen(),
+    //     ],)
+    //   ),
+    // );
   }
 }
