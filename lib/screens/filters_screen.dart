9f25770ea33fde616689c16fb6d272c0b9dcd7ad
Mark-DSouza <markdsouza434@gmail.com>
2021-04-13 10:59:12 +0530

    Add filter screen logic

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,19 +1,32 @@
-
 import 'package:flutter/material.dart';
 import '../widgets/main_drawer.dart';
 
 class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
 
+  final Function saveFilters;
+  final Map<String, bool> currentFilters;
+
+  FiltersScreen(this.currentFilters, this.saveFilters);
+
   @override
   _FiltersScreenState createState() => _FiltersScreenState();
 }
 
 class _FiltersScreenState extends State<FiltersScreen> {
-  var _glutenFree = false;
-  var _vegetarian = false;
-  var _vegan = false;
-  var _lactoseFree = false;
+  bool _glutenFree;
+  bool _vegetarian;
+  bool _vegan;
+  bool _lactoseFree;
+
+  @override
+  void initState() {
+    super.initState();
+    _glutenFree = widget.currentFilters['gluten'];
+    _vegetarian = widget.currentFilters['vegetarian'];
+    _vegan = widget.currentFilters['vegan'];
+    _lactoseFree = widget.currentFilters['lactose'];
+  }
 
   Widget _buildListViewTile(
       String title, String subtitle, bool currentValue, Function updateValue) {
@@ -28,65 +41,76 @@ class _FiltersScreenState extends State<FiltersScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Filters'),
-        ),
-        drawer: MainDrawer(),
-        body: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.all(20),
-              child: Text(
-                'Adjust your meal selection.',
-                style: Theme.of(context).textTheme.headline6,
-              ),
+      appBar: AppBar(title: Text('Filters'), actions: [
+        IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              final selectedFilters = {
+                'gluten': _glutenFree,
+                'lactose': _lactoseFree,
+                'vegan': _vegan,
+                'vegetarian': _vegetarian,
+              };
+              widget.saveFilters(selectedFilters);
+            }),
+      ]),
+      drawer: MainDrawer(),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection.',
+              style: Theme.of(context).textTheme.headline6,
             ),
-            Expanded(
-                child: ListView(
-              children: [
-                _buildListViewTile(
-                  'Gluten-free',
-                  'Only include gluten-free meals',
-                  _glutenFree,
-                  (newValue) {
-                    setState(() {
-                      _glutenFree = newValue;
-                    });
-                  },
-                ),
-                _buildListViewTile(
-                  'Vegetarian',
-                  'Only include vegetarian meals',
-                  _vegetarian,
-                  (newValue) {
-                    setState(() {
-                      _vegetarian = newValue;
-                    });
-                  },
-                ),
-                _buildListViewTile(
-                  'Vegan',
-                  'Only include vegan meals',
-                  _vegan,
-                  (newValue) {
-                    setState(() {
-                      _vegan = newValue;
-                    });
-                  },
-                ),
-                _buildListViewTile(
-                  'Lactose-free',
-                  'Only include lactose-free meals',
-                  _lactoseFree,
-                  (newValue) {
-                    setState(() {
-                      _lactoseFree = newValue;
-                    });
-                  },
-                ),
-              ],
-            ))
-          ],
-        ));
+          ),
+          Expanded(
+              child: ListView(
+            children: [
+              _buildListViewTile(
+                'Gluten-free',
+                'Only include gluten-free meals',
+                _glutenFree,
+                (newValue) {
+                  setState(() {
+                    _glutenFree = newValue;
+                  });
+                },
+              ),
+              _buildListViewTile(
+                'Vegetarian',
+                'Only include vegetarian meals',
+                _vegetarian,
+                (newValue) {
+                  setState(() {
+                    _vegetarian = newValue;
+                  });
+                },
+              ),
+              _buildListViewTile(
+                'Vegan',
+                'Only include vegan meals',
+                _vegan,
+                (newValue) {
+                  setState(() {
+                    _vegan = newValue;
+                  });
+                },
+              ),
+              _buildListViewTile(
+                'Lactose-free',
+                'Only include lactose-free meals',
+                _lactoseFree,
+                (newValue) {
+                  setState(() {
+                    _lactoseFree = newValue;
+                  });
+                },
+              ),
+            ],
+          ))
+        ],
+      ),
+    );
   }
 }
